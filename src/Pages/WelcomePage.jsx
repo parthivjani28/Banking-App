a5a4db047b8519aca4964b2330a2a2470017af26
Parthiv jani <parthivjani28@gmail.com>
2026-04-29 19:50:08 -0400

    Designing Layout

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import logo from '../logo.svg';
 
 function Welcome() {
     return (
         <> 
-        <div className="container m-auto">
-            <div className="card">
+        <div className="container d-flex wlcm-pg-div">
+            <div className="wlcm-pg-card text-center">
                 <div className="welcome-content">
+                    <img src={logo} className="react-logo" alt="react-logo" />
                     <h1>Welcome to the Banking Applications</h1>
-                    <button className="btn bg-light"><a href="/home"> Already an Exisiting Customer</a></button>
+                    <a href="/home" className="btn r-btn"> Already an Exisiting Customer</a>
                     <button className="btn bg-light"><a href="/register"> New Member? Join us today!</a></button>
 
                 </div>
